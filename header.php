eba29576ab8831028c6f63bd5f1e20a551eeed01
Mati <mati.ross@le-campus-numerique.fr>
2021-01-08 16:35:20 +0100

    Ajout des pages php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>CV Mati Ross</title>
+</head>
+
+<body>
+<header>
+    <div id="titre">
+        <p>Mati Ross<br>Apprenti Technicien Développeur</p>
+        <div class="nav">
+            <nav>
+                <ul>
+                    <li class="menu">▼Accueil
+                        <ul class="sous">
+                            <li><a href="index.html">Profil</a></li>
+                            <li><a href="hobby.html">Hobby</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div id="image">
+        <img src="images/photomati.jpg" alt="photo Mati Ross" class="photomati">
+    </div>
+</header>
---
+++ hobby.php
@@ -0,0 +1,21 @@
+<main>
+    <section class="hobbies">
+        <div>
+            <div id="titrehobby">
+                <h2>Voyages, voyages...</h2>
+            </div>
+        </div>
+        <div>
+            <div class="paragraphe">
+                <p>blablablablabla</p>
+            </div>
+        </div>
+        <div class="video-responsive">
+            <iframe width="1280" height="720" src="https://www.youtube.com/embed/JG209sKxjoY" frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+        </div>
+        <h2>Guitare</h2>
+        <p>Blablabla je joue de la guitare, j'ai modifié ma page hobby</p>
+    </section>
+</main>
