e067dc836ad669e5ac3cdd1b82688abd25f6bb8c
Mati <mati.ross@le-campus-numerique.fr>
2021-01-08 16:22:32 +0100

    Ajout de la page footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="droits">
+        <p>Campus numérique in the Alps - Valence</p>
+        <p>2020-2021</p>
+        <p>Dernières modifications le 07/01/2021</p>
+    </div>
+</footer>
